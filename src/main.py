6b1697e2d2e19af5a668de54bd08d052d78b3adb
Bayon-idris <bayonidris@gmail.com>
2026-01-06 02:18:35 +0800

    Load and Pre-trianed Yolo Model

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
-import cv2  
-import torch
+from ultralytics import YOLO
 
-
+def yolo_startup():
+    model = YOLO("yolov8n.pt")
+    results = model(
+        "D:/Personal Research/computer-vision-project/object-detection-using-yolo-pytorch/data/JPEGImages/2007_000033.jpg",
+        show=True
+    )
 
 def main():
-   print( torch.rand(5, 3))
+    yolo_startup()
 
 if __name__ == "__main__":
     main()
